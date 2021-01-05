d9e425ac058b5263d40aed03a1074b2f9ecdb8e9
Yasin Kaya <yasinkaya.121@gmail.com>
2021-01-06 00:16:44 +0300

    Setup basic project structure

---
+++ connection_manager/agent.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+from helper import send_at_com
+
+print(send_at_com("AT+CGDCONT?", "OK"))
---
+++ connection_manager/helper.py
@@ -0,0 +1,13 @@
+import subprocess
+
+def send_at_com(command, desired):
+    try:
+        cp = subprocess.run(["atcom", command, "--find", desired], universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        return (cp.stdout, cp.stderr, cp.returncode)
+    except Exception as e:
+        print(e) 
+        return False
+    
+    
+
+
