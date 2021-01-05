422ecfa82ad91309c34c02eba196da6a86da1a87
Yasin Kaya <yasinkaya.121@gmail.com>
2021-01-06 00:33:17 +0300

    Add logger

---
+++ connection_manager/logger.py
@@ -0,0 +1,30 @@
+import os
+import logging
+import logging.handlers
+
+logger = None
+
+def initialize_logger():
+    global logger
+    if logger:
+        return logger
+
+    logging_file_path = os.path.expanduser("~")+"/.sixfab/connect"
+    
+    if not os.path.exists(logging_file_path):
+        os.mkdir(logging_file_path)
+
+    logger = logging.getLogger("connection_manager")
+    logger.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter("%(asctime)s %(levelname)-8s %(message)s")
+    log_file_handler = logging.handlers.RotatingFileHandler(
+                                                            filename=logging_file_path+"cm-log", 
+                                                            maxBytes=10*1024*1024,
+                                                            backupCount=3
+                                                            )
+    log_file_handler.setFormatter(formatter)
+
+    logger.addHandler(log_file_handler)
+
+    return logger
