a93ff6c623c5a751f2e6b632ec2d73bc7ab78db0
Yasin Kaya <yasinkaya.121@gmail.com>
2021-01-14 22:55:41 +0300

    Add optional and required tags for modem id parameters

--- connection_manager/modules/identify.py
+++ connection_manager/modules/identify.py
@@ -14,7 +14,6 @@ system_id = {
     "arc" : "",
     "kernel" : "",
     "host_name" : "",
-    "modem_info" : "",
     "modem_vendor" : "",
     "modem_vendor_id" : "",
     "modem_product_id" : "",
@@ -24,7 +23,7 @@ system_id = {
 }
 
 
-def _turn_of_echo():
+def _turn_off_echo():
     output = send_at_com("ATE0", "OK")
 
     if output[2] == 0:
@@ -45,7 +44,8 @@ def _identify_vendor_name(method=0):
                 if output[0].find(vendor.name) != -1:
                     system_id["modem_vendor"] = vendor.name
                     step = 0    # Identification is successfull
-                    logger.debug("Modem vendor is detected with method 1!")
+                    method = 1  # Identification is successfull
+                    #logger.debug("Modem vendor is detected with method 1!")
 
             if system_id["modem_vendor"] == "":  
                 logger.warning("Modem vendor couldn't be found with method 1!")
@@ -53,7 +53,7 @@ def _identify_vendor_name(method=0):
         else:
             raise RuntimeError("Error occured on lsusb command!")
     
-    elif method == 0 or step == 2: 
+    if method == 0 or step == 2: 
         # [METHOD 2] By using usb-devices
         output = shell_command("usb-devices")
         if output[2] == 0:
@@ -61,7 +61,8 @@ def _identify_vendor_name(method=0):
                 if output[0].find(vendor.name) != -1:
                     system_id["modem_vendor"] = vendor.name
                     step = 0    # Identification is successfull
-                    logger.debug("Modem vendor is detected with method 2!")
+                    method = 1  # Identification is successfull
+                    #logger.debug("Modem vendor is detected with method 2!")
 
             if system_id["modem_vendor"] == "":
                 logger.warning("Modem vendor couldn't be found with method 2!")
@@ -69,19 +70,18 @@ def _identify_vendor_name(method=0):
         else:
             raise RuntimeError("Error occured on usb-devices command!")
     
-    elif method == 0 or step == 3:
+    if method == 0 or step == 3:
         # [METHOD 3] By using AT+GMI
         output = send_at_com("AT+GMI", "OK")
         if output[2] == 0:
             for vendor in ModemSupport.vendors:
                 if output[0].find(vendor.name) != -1:
                     system_id["modem_vendor"] = vendor.name
-                    step = 0    # Identification is successfull
-                    logger.debug("Modem vendor is detected with method 3!")
-
+                
             if system_id["modem_vendor"] == "":
                 logger.warning("Modem vendor couldn't be found with method 3!")
         else:
+            logger.warning("Modem vendor couldn't be found with method 3!")
             raise RuntimeError("Error occured on send_at_com --> AT+GMI command!")
 
     if system_id["modem_vendor"] == "":
@@ -104,7 +104,8 @@ def _identify_product_name(method=0):
                     if output[0].find(product_name) != -1:
                         system_id["modem_name"] = str(product_name)
                         step = 0    # Identification is successfull
-                        logger.debug("Modem name is detected with method 1!")
+                        method = 1  # Identification is successfull
+                        #logger.debug("Modem name is detected with method 1!")
 
             if system_id["modem_name"] == "":
                 logger.warning("Modem name couldn't be found with method 1!")
@@ -112,7 +113,7 @@ def _identify_product_name(method=0):
         else:
             raise RuntimeError("Error occured on usb-devices command!")
     
-    elif method == 0 or step == 2:
+    if method == 0 or step == 2:
         # [METHOD 2] By using AT+GMM
         output = send_at_com("AT+GMM", "OK")
         if output[2] == 0:
@@ -121,12 +122,12 @@ def _identify_product_name(method=0):
                     product_name = key.split("_")[0]
                     if output[0].find(product_name) != -1:
                         system_id["modem_name"] = str(product_name)
-                        step = 0    # Identification is successfull
-                        logger.debug("Modem name is detected with method 2!")
+                        #logger.debug("Modem name is detected with method 2!")
                     
             if system_id["modem_name"] == "":
                 logger.warning("Modem name couldn't be found with method 2!")
         else:
+            logger.warning("Modem name couldn't be found with method 2!")
             raise RuntimeError("Error occured on send_at_com --> AT+GMM command!")
 
     if system_id["modem_name"] == "":
@@ -134,7 +135,75 @@ def _identify_product_name(method=0):
     else:
         return system_id["modem_name"]
 
+def _identify_usb_vid_pid():
+    system_id["modem_vendor_id"] = ""
+    system_id["modem_product_id"] = ""
 
+    output = shell_command("usb-devices")
+    if output[2] == 0:
+        for vendor in ModemSupport.vendors:
+            if output[0].find(vendor.vendor_id) != -1:
+                system_id["modem_vendor_id"] = vendor.vendor_id
+                
+        for vendor in ModemSupport.vendors:
+            for key in vendor.modules:
+                if output[0].find(vendor.modules[key]) != -1:
+                    system_id["modem_product_id"] = str(vendor.modules[key])
+            
+        if system_id["modem_vendor_id"] == "" or system_id["modem_product_id"] == "":
+            raise ModemNotSupported("Modem is not supported!")
+        else:
+            return (system_id["modem_vendor_id"], system_id["modem_product_id"])
+    else:
+        raise RuntimeError("Error occured on usb-devices command!")
+
+def _identify_imei():
+    output = send_at_com("AT+CGSN","OK")
+    raw_imei = output[0] if output[2] == 0 else "" 
+    
+    if raw_imei != "":
+        imei_filter = filter(str.isdigit, raw_imei)
+        system_id["imei"] = "".join(imei_filter)
+        return system_id["imei"]
+    else: 
+        raise ModemNotReachable("IMEI couldn't be detected!")
+
+def _identify_fw_version():
+    output = send_at_com("AT+CGMR","OK")
+    system_id["sw_version"] = output[0].split("\n")[1] if output[2] == 0 else ""
+    
+    if system_id["sw_version"] != "":
+        return system_id["sw_version"]
+    else: 
+        raise ModemNotReachable("Firmware Ver. couldn't be detected!")
+
+def _identify_ccid():
+    output = send_at_com("AT+CCID","OK")
+    raw_ccid = output[0] if output[2] == 0 else ""
+
+    if raw_ccid != "":
+        ccid_filter = filter(str.isdigit, raw_ccid)
+        system_id["ccid"] = "".join(ccid_filter)
+        return system_id["ccid"]
+    else: 
+        raise ModemNotReachable("CCID couldn't be detected!")
+
+def _identify_os():
+    try:
+        logger.debug("[+] OS artchitecture")
+        system_id["arc"] = str(platform.architecture()[0])
+        
+        logger.debug("[+] Kernel version")
+        system_id["kernel"] = str(platform.release())
+        
+        logger.debug("[+] Host name")
+        system_id["host_name"] = str(platform.node())
+        
+        logger.debug("[+] OS platform")
+        system_id["platform"] = str(platform.platform())
+    except Exception as e:
+        raise RuntimeError("Error occured while getting OS identification!")
+    
 def identify_setup():
     logger.info("[?] System identifying...")
     
@@ -151,97 +220,51 @@ def identify_setup():
     try:
         _identify_product_name()
     except Exception as e:
-        logger.critical("Modem name identification failed!")
+        logger.warning("Modem name identification failed!")
+        system_id["modem_name"] = "Unknown"
+
+    # Vendor ID & Product ID (Required)
+    logger.debug("[+] Modem USB VID and PID")
+    try:
+        _identify_usb_vid_pid()
+    except Exception as e:
+        logger.warning("Modem VID PID identification failed!")
         raise ModemNotSupported("Modem is not supported!")
 
-
-    print(system_id["modem_vendor"])
-    print(system_id["modem_name"])
-    exit()
-
-    # Vendor ID & Product ID
-    logger.debug("[+] Modem vendor id and product id")
-    system_id["modem_product_id"] = ""
-    output = shell_command("usb-devices")
-
-    if output[2] == 0:
-        for vendor in ModemSupport.vendors:
-            if output[0].find(vendor.vendor_id) != -1:
-                system_id["modem_vendor_id"] = vendor.vendor_id
-                
-        if system_id["modem_vendor_id"] == "":  
-            raise ModemNotSupported("Modem is not supported!")
-
-        for vendor in ModemSupport.vendors:
-            for key in vendor.modules:
-                if output[0].find(vendor.modules[key]) != -1:
-                    system_id["modem_product_id"] = str(vendor.modules[key])
-            
-        if system_id["modem_product_id"] == "":
-            raise ModemNotSupported("Modem is not supported!")
-
-    else:
-        raise RuntimeError("Error occured on usb-devices command!")
-
     
-    # Modem Info Text
-    logger.debug("[+] Modem info")
-    system_id["modem_info"] = ""
-    output = send_at_com("AT+GMM", "OK")
-    
-    if output[2] == 0:
-        for vendor in ModemSupport.vendors:
-            for key in vendor.modules:
-                product_name = key.split("_")[0]
-                if output[0].find(product_name) != -1:
-                    #print(product_name)
-                    system_id["modem_info"] = system_id["modem_vendor"] + " " + product_name
-                
-        if system_id["modem_info"] == "":
-            raise ModemNotSupported("Modem is not supported!")
-    else:
-        raise RuntimeError("Error occured on usb-devices command!")
-
-    
-    # IMEI
+    # IMEI (Optional)
     logger.debug("[+] IMEI")
-    output = send_at_com("AT+CGSN","OK")
-    raw_imei = output[0] if output[2] == 0 else "" 
-    imei_filter = filter(str.isdigit, raw_imei)
-    system_id["imei"] = "".join(imei_filter)
-    
-    
-    # SW version
-    logger.debug("[+] Modem firmware revision")
-    output = send_at_com("AT+CGMR","OK")
-    system_id["sw_version"] = output[0].split("\n")[1] if output[2] == 0 else ""
-
-    # SIM identification
-    # -----------------------------------------
-    # CCID
-    logger.debug("[+] SIM UCCID")
-    output = send_at_com("AT+CCID","OK")
-    raw_ccid = output[0] if output[2] == 0 else ""
-    ccid_filter = filter(str.isdigit, raw_ccid)
-    system_id["ccid"] = "".join(ccid_filter)
-
-    # OS identification
-    # -----------------------------------------
     try:
-        logger.debug("[+] OS artchitecture")
-        system_id["arc"] = str(platform.architecture()[0])
-        
-        logger.debug("[+] Kernel version")
-        system_id["kernel"] = str(platform.release())
-        
-        logger.debug("[+] Host name")
-        system_id["host_name"] = str(platform.node())
-        
-        logger.debug("[+] OS platform")
-        system_id["platform"] = str(platform.platform())
+        _identify_imei()
     except Exception as e:
-        logger.error("Error occured while getting OS identification!")
-        raise RuntimeError("Error occured while getting OS identification!")
+        logger.warning("IMEI identification failed!")
+        system_id["imei"] = "Unknown"
+    
+    
+    # SW version (Optional)
+    logger.debug("[+] Modem firmware revision")
+    try:
+        _identify_fw_version()
+    except Exception as e:
+        logger.warning("Modem firmware ver. identification failed!")
+        system_id["sw_version"] = "Unknown"
+
+    
+    # CCID (Optional)
+    logger.debug("[+] SIM UCCID")
+    try:
+        _identify_ccid()
+    except Exception as e:
+        logger.warning("SIM CCID identification failed!")
+        system_id["ccid"] = "Unknown"
+
+    # OS (Optional)
+    logger.debug("[>] OS Identification")
+    try:
+        _identify_os()
+    except Exception as e:
+        logger.warning("OS identification failed!")
+    
 
     if DEBUG == True and VERBOSE_MODE == True:
         print("")
@@ -252,16 +275,6 @@ def identify_setup():
             print(str("[+] " + x[0]) + " --> " + str(x[1]))
         print("********************************************************************")
         print("")
-
-
-    for key in system_id:
-        if system_id[key] == "":
-            logger.error("Identification failed!")
-
-            if key == "ccid":
-                logger.error("SIM couldn't be identified!")
-
-            raise RuntimeError("Identification failed!")
 
     try:
         write_yaml_all(SYSTEM_PATH, system_id)
