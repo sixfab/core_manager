5c07acdb9f969c4fa908714328917f3e5e9f5e28
Yasin Kaya <yasinkaya.121@gmail.com>
2021-03-17 18:34:24 +0300

    fix config parser

--- core_manager/helpers/config_parser.py
+++ core_manager/helpers/config_parser.py
@@ -21,8 +21,8 @@ else:
 default_config = {
     "apn" : connect_dict.get("apn", "super"),
 
-    "debug_mode" : True,
-    "verbose_mode" : True,
+    "debug_mode" : False,
+    "verbose_mode" : False,
 
     "check_internet_interval" : 60,
     "send_monitoring_data_interval" : 60,
@@ -36,30 +36,23 @@ default_config = {
 
 # Check the config file exist.
 if os.path.isfile(CONFIG_PATH):
-    pass
+    try:
+        config = read_yaml_all(CONFIG_PATH)
+    except Exception as e:
+        print(e) # debug
 else:
     print("Config file doesn't exist! Restoring default configs...") # debug
-    try:
-        config = default_config
-        write_yaml_all(CONFIG_PATH, config)
-    except Exception as e:
-        print("Error occured when default config file is creating!") # debug
+    config = default_config
 
-try:
-    config = read_yaml_all(CONFIG_PATH)
-except Exception as e:
-    print(e) # debug
-
-
-VERBOSE_MODE = config.get("verbose_mode", False)
-DEBUG = config.get("debug_mode", False)
-APN = config.get("apn", "super")
-PING_TIMEOUT = config.get("ping_timeout", 9)
-INTERVAL_CHECK_INTERNET = config.get("check_internet_interval", 60)
-INTERVAL_SEND_MONITOR = config.get("send_monitoring_data_interval", 60)
-INTERVAL_MANAGE_NETWORK = config.get("send_monitoring_data_interval", 60)
-NETWORK_NAME = config.get("network_name")
-NETWORK_PRIORTY = config.get("network_priority")
+VERBOSE_MODE = config.get("verbose_mode", default_config.get("verbose_mode"))
+DEBUG = config.get("debug_mode", default_config.get("debug_mode"))
+APN = config.get("apn", default_config.get("apn"))
+PING_TIMEOUT = config.get("ping_timeout", default_config.get("ping_timeout"))
+INTERVAL_CHECK_INTERNET = config.get("check_internet_interval", default_config.get("check_internet_interval"))
+INTERVAL_SEND_MONITOR = config.get("send_monitoring_data_interval", default_config.get("send_monitoring_data_interval"))
+INTERVAL_MANAGE_NETWORK = config.get("send_monitoring_data_interval", default_config.get("send_monitoring_data_interval"))
+NETWORK_NAME = config.get("network_name", default_config.get("network_name"))
+NETWORK_PRIORTY = config.get("network_priority", default_config.get("network_priority"))
 
 logger = initialize_logger(DEBUG)
 
