be6f353d775af222f1b361403dce415e4924211e
Yasin Kaya <yasinkaya.121@gmail.com>
2023-01-14 11:23:24 +0300

    hotfix: fix only soft reboot issue (#88)
    * feat(network-manager): Dynamic if priority by type (#80)
    
    * chore(network-manager): Add feature of assigning interface priority by interface type -->
    
    * Add new priority mapping
    * Add InterfaceTypes class
    * Change default_config
    * And lots of little changes...
    
    * chore: Separate connection status check and monitoring functions
    
    * chore(network-manager): Fix some bugs thats is detected on testing
    
    * chore: Update version to v1.10.0-beta.1
    
    * feat: Add interface types to monitoring data
    
    * refactor: Delete debugging purposes lines
    
    * fix: diagnostic feature (#82)
    
    * fix(modem): Fix bug that preventing  hard_reboot
    
    * refactor(diagnostic): Improve diagnostic module
    
    * fix(monitor): Fix unintended zero bug of incident count (#81)
    
    * chore(network-manager): Add feature of assigning interface priority by interface type -->
    
    * Add new priority mapping
    * Add InterfaceTypes class
    * Change default_config
    * And lots of little changes...
    
    * chore: Separate connection status check and monitoring functions
    
    * chore(network-manager): Fix some bugs thats is detected on testing
    
    * chore: Update version to v1.10.0-beta.1
    
    * fix(monitor): Fix bug which consisted incident count being zero when the manager service restarting
    
    * refactor(diagnostics): Add timestamp data to diagnostic reports (#83)
    
    The timestamp field will be updated only if zipped diagnostic data or incident_id is changed
    
    * fix(geolocation): Clear buffer before reading new geolocation data (#84)
    
    * fix(geolocation): Clear buffer before reading new geolocation data
    
    * refactor: Update version to v1.10.1-beta.1
    
    * Fix incidents by using hard reboot when it is necessary
    
    * Try some fix
    
    * Set checking network registeration timeout 15 mins
    
    * Update version to 1.9.5-beta.1
    
    * Fix non-type sbc error
    
    * Update version to v1.9.5
    
    Co-authored-by: Ensar <ensarkarabudak@gmail.com>
    Co-authored-by: Buğra İşgüzar <biisguzar@gmail.com>

--- core_manager/nm.py
+++ core_manager/nm.py
@@ -8,13 +8,31 @@ def manage_network(modem):
     network = Network(modem)
     network.check_interfaces()
     network.get_interface_metrics()
-    network.check_and_create_monitoring()
+    network.get_interface_type()
+    network.check_connection_status()
 
     try:
         network.adjust_priorities()
     except Exception as error:
         logger.error("adjust_priorities() --> %s", error)
 
+    network.get_interface_priority()
+    network.create_monitoring_data()
     network.debug_routes()
 
     return network
+
+if __name__ == "__main__":
+    import time
+    modem = Telit()
+    while True:
+        network = Network(modem)
+        network.check_interfaces()
+        network.get_interface_metrics()
+        network.get_interface_type()
+        network.check_connection_status()
+        network.adjust_priorities()
+        network.get_interface_priority()
+        network.create_monitoring_data()
+        network.debug_routes()
+        time.sleep(5)
