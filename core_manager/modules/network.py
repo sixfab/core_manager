a38b277150cf889ca768058e40957d7b4d9e7f14
Yasin Kaya <yasinkaya.121@gmail.com>
2023-12-07 21:04:18 +0300

    Fix permission issue

--- core_manager/modules/network.py
+++ core_manager/modules/network.py
@@ -277,7 +277,7 @@ class Network():
         """
         dns_servers = ["8.8.8.8", "8.8.4.4"]
         dns_lines = ["nameserver 8.8.8.8", "nameserver 8.8.4.4"]
-        dns_file = "/etc/resolv.conf"
+        dns_file = "/temp/resolv.conf"
 
         if os.path.exists(dns_file):
             with open(dns_file, 'r') as file:
@@ -287,6 +287,14 @@ class Network():
                 if dns_server not in org_resolv_conf:
                     with open(dns_file, 'w') as file:
                         file.write(dns_lines[0] + "\n" + dns_lines[1] + "\n" + org_resolv_conf)
+            
+                    output = shell_command("sudo mv /temp/resolv.conf /etc/resolv.conf")
+
+                    if output[2] == 0:
+                        logger.info("DNS servers are OK!")
+                    else:
+                        RuntimeError('Error occured updating resolv.conf')
+
 
     def create_monitoring_data(self):
         self.monitor.clear()
